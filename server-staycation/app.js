92252f0c1e9c305557647f444ef021e799f11ca4
Faris Abdurrahman <faris.code@gmail.com>
2020-11-23 14:52:04 +0700

    API Mostpicked and Category

--- server-staycation/app.js
+++ server-staycation/app.js
@@ -47,7 +47,7 @@ app.use('/', indexRouter)
 app.use('/users', usersRouter)
 // admin router
 app.use('/admin', adminRouter)
-app.use('/api/v1', apiRouter)
+app.use('/api/v1/member', apiRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- server-staycation/controller/apiController.js
+++ server-staycation/controller/apiController.js
@@ -1,14 +1,43 @@
 const Item = require('../models/Item')
+const Treasure = require('../models/Activity')
+const Traveler = require('../models/Booking')
+const Category = require('../models/Category')
 
 module.exports = {
     landingPage: async (req, res) => {
         try {
             const mostPicked = await Item.find()
-                .select('_id title country city price unit imageId')   
+                .select('_id title country city price unit imageId')
                 .limit(5)
-            res.status(200).json({ mostPicked })
+                .populate({ path: 'imageId', select: '_id imageUrl' })
+
+            const category = await Category.find()
+                .select('_id name')
+                .limit(3)
+                .populate({
+                    path: 'itemId',
+                    select: '_id title country city isPopular imageId',
+                    populate: {
+                        path: 'imageId',
+                        select: '_id imageUrl',
+                        perDocumentLimit: 1
+                    }
+                })
+
+            const traveler = await Traveler.find()
+            const treasure = await Treasure.find()
+            const city = await Item.find()
+            res.status(200).json({
+                hero: {
+                    travelers: traveler.length,
+                    treasures: treasure.length,
+                    cities: city.length
+                },
+                mostPicked,
+                category
+            })
         } catch (error) {
-            
+
         }
     }
 }
