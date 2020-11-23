2f707953cf2ccfe20e20f86facfdba062bbabf50
Faris Abdurrahman <faris.code@gmail.com>
2020-11-23 12:45:56 +0700

    action accept and reject booking

--- server-staycation/controller/adminController.js
+++ server-staycation/controller/adminController.js
@@ -588,14 +588,62 @@ module.exports = {
 				.populate('memberId')
 				.populate('bankId')
 
-				console.log(booking)
 			res.render('admin/booking/view_booking', {
 				title: "Staycation | Booking",
 				user: req.session.user,
 				booking
 			})
 		} catch (error) {
-			
+			res.redirect('/admin/booking')
+		}
+	},
+
+	showDetailBooking: async (req, res) => {
+		const { id } = req.params
+		try {
+			const alertMessage = req.flash('alertMessage')
+			const alertStatus = req.flash('alertStatus')
+			const alert = { message: alertMessage, status: alertStatus }
+
+			const booking = await Booking.findOne({ _id: id })
+				.populate('memberId')
+				.populate('bankId')
+			res.render('admin/booking/show_detail_booking', {
+				title: "Staycation | Detail Booking",
+				user: req.session.user,
+				booking,
+				alert
+			})
+		} catch (error) {
+			res.redirect('/admin/booking')
+		}
+	},
+
+	actionConfirmation: async (req, res) => {
+		const { id } = req.params
+		try {
+			const booking = await Booking.findOne({ _id: id })
+			booking.payments.status = 'Accepted'
+			await booking.save()
+
+			req.flash('alertMessage', 'Success Accept Confirmation!')
+			req.flash('alertStatus', 'success')
+			res.redirect(`/admin/booking/${id}`)
+		} catch (error) {
+			res.redirect(`/admin/booking/${id}`)
+		}
+	},
+	actionReject: async (req, res) => {
+		const { id } = req.params
+		try {
+			const booking = await Booking.findOne({ _id: id })
+			booking.payments.status = 'Rejected'
+			await booking.save()
+			req.flash('alertMessage', 'Success Reject Confirmation!')
+			req.flash('alertStatus', 'success')
+			res.redirect(`/admin/booking/${id}`)
+		} catch (error) {
+			res.redirect(`/admin/booking/${id}`)
 		}
 	}
 }
