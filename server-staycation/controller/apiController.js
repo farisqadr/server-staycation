1b601b02192583bd7fd0e1578795254821689631
Faris Abdurrahman <faris.code@gmail.com>
2020-11-23 16:41:03 +0700

    API isPopular and Testimonials

--- server-staycation/controller/apiController.js
+++ server-staycation/controller/apiController.js
@@ -17,6 +17,10 @@ module.exports = {
                 .populate({
                     path: 'itemId',
                     select: '_id title country city isPopular imageId',
+                    perDocumentLimit: 4,
+                    option: {
+                        sort: { sumBooking: -1 }
+                    },
                     populate: {
                         path: 'imageId',
                         select: '_id imageUrl',
@@ -27,6 +31,29 @@ module.exports = {
             const traveler = await Traveler.find()
             const treasure = await Treasure.find()
             const city = await Item.find()
+
+            for (let i = 0; i < category.length; i++) {
+                for (let x = 0; x < category[i].itemId.length; x++) {
+                    const item = await Item.findOne({ _id: category[i].itemId[x]._id })
+                    item.isPopular = false
+                    await item.save()
+                    if (category[i].itemId[0] === category[i].itemId[x]) {
+                        item.isPopular = true
+                        await item.save()
+                    }
+                }
+            }
+
+            const testimonial = {
+                _id: "asd1293uasdads1",
+                imageUrl: "images/testimonial1.jpg",
+                name: "Happy Family",
+                rate: 4.55,
+                content: "What a great trip with my family and I should try again next time soon ...",
+                familyName: "Angga",
+                familyOccupation: "Product Designer"
+            }
+
             res.status(200).json({
                 hero: {
                     travelers: traveler.length,
@@ -34,10 +61,12 @@ module.exports = {
                     cities: city.length
                 },
                 mostPicked,
-                category
+                category,
+                testimonial
             })
         } catch (error) {
-
+            console.log(error)
+            req.status(500).json({ message: "Internal server error" })
         }
     }
 }
